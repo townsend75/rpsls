f3d355cedc9c70e012b975412bc72f7d404257b7
townsend75 <>
2023-07-29 16:04:34 +0000

    add js as in tutorial

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,29 @@
- let questions = {
-    "What is the capital city of Ecuador?" : [ "Bogota", "Qutio", "Lima", "Caracas", 1],
-    "In which US city would you find the Empire State Building?" : [ "New Orleans", "New York", "Chicago", "Los Angeles", 1],
-    "On which continent would you find the Victoria Falls?" : ["Europe", "South America", "Asia","Africa", 3],
-    "Tahiti lies in which ocean?" : ["Atlantic", "Indian", "Pacific", "Antarctic", 2 ]
+const buttons= document.getElementsByClassName("control");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const playerImage = document.getElementById("player-image);
+const computerImage = document.getElementById("computer-image");
+const messages = document.getElementById("messages");
+const choices = [ "Rock", "Paper", "Scissors", "Lizard", "Spock" ]
+
+for (let button of buttons) {
+    button.addEventListener (click, function() {
+        let playerChoice = this.gameAttribute("data-choice");
+        playGame(playerChoice);
+    });
+
 }
 
-function getRandomQuestion() {
-    var randomIndex = Math.floor(Math.random() * questions.length);
-    return questions[randomIndex];
-  }
-  
-  // Function to display the quiz
-  function displayQuiz(maxQuestions) {
-    var quiz = document.getElementById("quiz");
-    var numQuestionsDisplayed = 0;
-  
-    // Loop until max number of questions are displayed
-    while (numQuestionsDisplayed < maxQuestions) {
-      var question = getRandomQuestion();
-  
-      // Add the question to the quiz
-      var questionDiv = document.createElement("div");
-      questionDiv.innerHTML = question;
-      quiz.appendChild(questionDiv);
-  
-      // Increment the number of questions displayed
-      numQuestionsDisplayed++;
-    }
-  }
-  
-  // Call the function to display the quiz (10 questions in this example)
-  displayQuiz(10);
+function playGame (playerChoice) {
+playerImage.src = `assets/images/${choices(playerChoice)}.png`;
+playerImage.alt = choices[playerChoice];
+
+let computerChoice = Math.floor(Math.random () * 3);
+
+computerImage.src = `assets/images/${choices(computerChoice)}.png`;
+computerImage.alt = choice[compuetChoice];
+
+let result = checkWinner( choices[computerChoice, choices[playerChoice]]);
+
+updateScores(result);
+}
