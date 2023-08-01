9d615aba272db2e7ab707abd6140abda3e1d3e85
townsend75 <>
2023-08-01 13:07:31 +0000

    change colour of header and footer. Begin work on readme file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,41 @@
-const buttons= document.getElementsByClassName("control");
+const buttons = document.getElementsByClassName("control");
 const playerScore = document.getElementById("player-score");
 const computerScore = document.getElementById("computer-score");
 const playerImage = document.getElementById("player-image");
 const computerImage = document.getElementById("computer-image");
 const messages = document.getElementById("messages");
-const choices = [ "rock", "paper", "scissors", "lizard", "spock" ];
-const outcomes = choices[[0,2,1,1,2],[1,0,2,2,1],[2,1,0,1,2],[2,1,2,0,1],[1,2,1,2,0]];
-const images = ["rock.png", "paper.png", "scissors.png", "lizard.png","spock.png"]
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+const images = ["rock.png", "paper.png", "scissors.png", "lizard.png", "spock.png"]
 
 
 for (let button of buttons) {
-    button.addEventListener ("click", function(event) {
+    button.addEventListener("click", function (event) {
         let playerChoice = parseInt(this.dataset.choice);
         playGame(playerChoice);
     });
 
 }
 
-function playGame (playerChoice) {
-playerImage.src = `assets/images/${choices[playerChoice]}.png`;
-playerImage.alt = choices[playerChoice];
+function playGame(playerChoice) {
+    playerImage.src = `assets/images/${choices[playerChoice]}.png`;
+    playerImage.alt = choices[playerChoice];
 
-let computerChoice = Math.floor(Math.random () * 5);
-computerImage.src = `assets/images/${choices[computerChoice]}.png`;
-computerImage.alt = choices[computerChoice];
+    let computerChoice = Math.floor(Math.random() * 5);
+    computerImage.src = `assets/images/${choices[computerChoice]}.png`;
+    computerImage.alt = choices[computerChoice];
 
-let result = checkWinner(choices[playerChoice], choices[computerChoice]);
+    let result = checkWinner(choices[playerChoice], choices[computerChoice]);
 
-/*updateScores(result);*/
+   
 }
+/*function isGameOver() {
+    if (playerScore === 10) {
+        alert("Congrats, you win!");
+    } else if (computerScore === 10) {
+        alert("Bad luck, you lose!");
+    } else {}
+    
+}*/
 
 function playerCounter() {
     let oldScore = parseInt(document.getElementById("player-score").innerText);
@@ -41,17 +48,65 @@ function computerCounter() {
 }
 
 function checkWinner(choice1, choice2) {
-    
-       if (choice1 === choice2) {
-        alert ("It's a tie!")    
-       }
-                       if (choice1 === "rock" && choice2 == "paper" || choice2 == "spock") {
-                        alert("You lose");
-                        computerCounter();
-                        
-                          }
-                         else if (choice1 === "rock" && choice2 == "scissors" || choice2 == "lizard") {
-                            alert ("You win");
-                       playerCounter();
-                 }
-                }
+
+    if (choice1 === choice2) {
+         messages.textContent = "It's a tie";
+    }
+    else if (choice1 == "rock" && choice2 == "paper" || choice2 == "spock") {
+        
+        computerCounter();
+        messages.textContent = "Computer wins!";
+
+    }
+    else if (choice1 == "rock" && choice2 == "scissors" || choice2 == "lizard") {
+        
+        playerCounter();
+        messages.textContent  = "You win!";
+       
+    }
+   else if (choice1 == "paper" && choice2 == "scissors" || choice2 == "lizard") {
+       
+        computerCounter();
+        messages.textContent = "Computer wins!";
+      
+    } else if (choice1 == "paper" && choice2 == "rock" || choice2 == "spock") {
+        
+        playerCounter();
+        messages.textContent  = "You win!";
+       
+    }
+    else if (choice1 == "scissors" && choice2 == "rock" || choice2 == "spock") {
+        
+        computerCounter();
+        messages.textContent = "Computer wins!";
+      
+    } else if (choice1 == "scissors" && choice2 == "paper" || choice2 == "lizard") {
+       
+        playerCounter();
+        messages.textContent  = "You win!";
+       
+    }
+    else if (choice1 == "lizard" && choice2 == "scissors" || choice2 == "rock") {
+      
+        computerCounter();
+        messages.textContent = "Computer wins!";
+       
+    } else if (choice1 == "lizard" && choice2 == "paper" || choice2 == "spock") {
+       
+        playerCounter();
+        messages.textContent  = "You win!";
+        
+    }
+    else if (choice1 == "spock" && choice2 == "lizard" || choice2 == "paper") {
+       
+        computerCounter();
+        messages.textContent = "Computer wins!";
+      
+    } else if (choice1 == "spock" && choice2 == "scissors" || choice2 == "rock") {
+       
+        playerCounter();
+        messages.textContent  = "You win!";
+      
+    }
+}
+
---
+++ readme.md
@@ -0,0 +1,26 @@
+ # Rock, Paper,Scissors, Lizard,Spock- the BigBang Game
+
+ Welcome to rock, paper scissors on steroids! INspired by the popular TV show "The Big Bang Theory", this game adds two new elements to the classic game, providing loads of different outcomes! Here you play against a computer which randomly chooses one of five possibilites- rock, paper, scissors, lizard or spock. Each choice can defeat two of the other options, will lose to two options and will obviously tie against an identical choice. 
+
+
+ ## Table of Contents
+
+ ### Features
+ ### UX/ UI
+ ### Testing
+ ### Deployment
+ ### Sources
+ ### Future Considerations
+
+
+ ### Features
+
+ Since the main aim of this project was to implement javascript, the basic design of the site is very simplistic. THe header and footer have identical background colours which give a symetrical appearance without making the text difficult to read. 
+
+ In the game area, there are five buttons which are clearly marked for the pkayer to make their choice. Each choice reveals an image of the player's choice and the corresponding image of the computer's (random) choice. A message is shown displaying the result  and the scores for player and computer are tallied within the game areas. 
+
+
+
+
+
+
