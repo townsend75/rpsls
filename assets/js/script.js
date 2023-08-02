614182761a2a793cea21defc1624a647c1ad9a20
townsend75 <>
2023-08-02 11:10:50 +0000

    add game tally feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,21 +24,25 @@ function playGame(playerChoice) {
     computerImage.src = `assets/images/${choices[computerChoice]}.png`;
     computerImage.alt = choices[computerChoice];
 
-     checkWinner(choices[playerChoice], choices[computerChoice]);
+    checkWinner(choices[playerChoice], choices[computerChoice]);
 
-   
+
 }
 function isGameOver() {
     if (playerScore.textContent == 10) {
         alert("Congrats, you win!");
-        window.location.reload();
-       
+        playerTally();
+        playerScore.textContent = 0;
+        computerScore.textContent = 0;
+
     } else if (computerScore.textContent == 10) {
         alert("Bad luck, you lose!")
-        window.location.reload();
-       
-    } else {}
-    
+        computerTally();
+        computerScore.textContent = 0;
+        playerScore.textContent = 0;
+
+    } else { }
+
 }
 
 function playerCounter() {
@@ -51,76 +55,86 @@ function computerCounter() {
     document.getElementById("computer-score").innerText = ++oldScore;
 }
 
+function playerTally() {
+    let oldScore = parseInt(document.getElementById("player-tally").innerText);
+    document.getElementById("player-tally").innerText = ++oldScore;
+}
+
+function computerTally() {
+    let oldScore = parseInt(document.getElementById("computer-tally").innerText);
+    document.getElementById("computer-tally").innerText = ++oldScore;
+
+}
 function checkWinner(choice1, choice2) {
 
     if (choice1 === choice2) {
-         messages.textContent = "It's a tie";
-         isGameOver();
+        messages.textContent = "It's a tie";
+        isGameOver();
     }
     else if (choice1 == "rock" && choice2 == "paper" || choice2 == "spock") {
-        
+
         computerCounter();
         messages.textContent = "Computer wins!";
         isGameOver();
 
     }
     else if (choice1 == "rock" && choice2 == "scissors" || choice2 == "lizard") {
-        
+
         playerCounter();
-        messages.textContent  = "You win!";
+        messages.textContent = "You win!";
         isGameOver();
-       
+
     }
-   else if (choice1 == "paper" && choice2 == "scissors" || choice2 == "lizard") {
-       
+    else if (choice1 == "paper" && choice2 == "scissors" || choice2 == "lizard") {
+
         computerCounter();
         messages.textContent = "Computer wins!";
         isGameOver();
-      
+
     } else if (choice1 == "paper" && choice2 == "rock" || choice2 == "spock") {
-        
+
         playerCounter();
-        messages.textContent  = "You win!";
+        messages.textContent = "You win!";
         isGameOver();
-       
+
     }
     else if (choice1 == "scissors" && choice2 == "rock" || choice2 == "spock") {
-        
+
         computerCounter();
         messages.textContent = "Computer wins!";
         isGameOver();
-      
+
     } else if (choice1 == "scissors" && choice2 == "paper" || choice2 == "lizard") {
-       
+
         playerCounter();
-        messages.textContent  = "You win!";
+        messages.textContent = "You win!";
         isGameOver();
-       
+
     }
     else if (choice1 == "lizard" && choice2 == "scissors" || choice2 == "rock") {
-      
+
         computerCounter();
         messages.textContent = "Computer wins!";
         isGameOver();
-       
+
     } else if (choice1 == "lizard" && choice2 == "paper" || choice2 == "spock") {
-       
+
         playerCounter();
-        messages.textContent  = "You win!";
+        messages.textContent = "You win!";
         isGameOver();
     }
     else if (choice1 == "spock" && choice2 == "lizard" || choice2 == "paper") {
-       
+
         computerCounter();
         messages.textContent = "Computer wins!";
         isGameOver();
-      
+
     } else if (choice1 == "spock" && choice2 == "scissors" || choice2 == "rock") {
-       
+
         playerCounter();
-        messages.textContent  = "You win!";
+        messages.textContent = "You win!";
         isGameOver();
-      
+
     }
 }
 
